346747a3f6159e6779246b4d8b20ded272151592
Eyvette Thompson <eyvettet@gmail.com>
2017-10-05 14:57:46 -0400

    Add files via upload

---
+++ chapter__2_lab.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Doing some math</title>
+    
+</head>
+<body>
+    <p>
+        <?php print("27 * 3 = " . (27*3)); ?><br/>
+        <?php print("19 / 5 = " . (19/5)); ?><br/>
+        <?php print("22 + 3 + 21 = " . (22+3+21)); ?><br/>
+        <?php print("18 + 2 * 2 = " . (18+2*2)); ?><br/>
+        
+    </p>    
+        
+</body>
+ 
+</html>
